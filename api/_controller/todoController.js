698db99a46b67e050dbd6ad825e3540eacb12674
Posmj <psyjma1201@gmail.com>
2023-05-23 13:51:34 +0900

    10주차 login

--- api/_controller/todoController.js
+++ api/_controller/todoController.js
@@ -1,47 +1,19 @@
 const db = require("../../plugins/mysql");
 const TABLE = require("../../util/TABLE");
 const STATUS = require("../../util/STATUS");
-const { resData, isEmpty } = require("../../util/lib");
+const { resData, currentTime, isEmpty } = require("../../util/lib");
 const moment = require("../../util/moment");
 
 //전체 row 갯수
 const getTotal = async () => {
+  // const getTotal = async function () {
   try {
-    const query = `SELECT COUNT(*) AS cnt FROM ${TABLE.TODO} WHERE done='N'`;
+    const query = `SELECT COUNT(*) AS cnt FROM ${TABLE.TODO}`;
     const [[{ cnt }]] = await db.execute(query);
     return cnt;
   } catch (e) {
     console.log(e.message);
-    return resData(
-      STATUS.E300.result,
-      STATUS.E300.resultDesc,
-      moment().format("LT")
-    );
-  }
-};
-
-// 페이징으로 가져오기
-const getList = async (req) => {
-  try {
-    // 마지막 id, len 갯수
-    const lastId = parseInt(req.query.lastId) || 0;
-    const len = parseInt(req.query.len) || 10;
-
-    let where = "";
-    if (lastId) {
-      // 0은 false
-      where = ` AND id < ${lastId}`;
-    }
-    const query = `SELECT * FROM ${TABLE.TODO} WHERE done='N' ${where} order by id desc limit 0, ${len}`;
-    const [rows] = await db.execute(query);
-    return rows;
-  } catch (e) {
-    console.log(e.message);
-    return resData(
-      STATUS.E300.result,
-      STATUS.E300.resultDesc,
-      moment().format("LT")
-    );
+    return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
   }
 };
 
@@ -55,68 +27,70 @@ const getSelectOne = async (id) => {
     return cnt;
   } catch (e) {
     console.log(e.message);
-    return resData(
-      STATUS.E300.result,
-      STATUS.E300.resultDesc,
-      moment().format("LT")
-    );
+    return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
+  }
+};
+
+// 페이징으로 가져오기
+const getList = async (req) => {
+  try {
+    // 마지막 id, len 갯수
+    const lastId = parseInt(req.query.lastId) || 0;
+    const len = parseInt(req.query.len) || 10;
+
+    let where = "";
+    if (lastId) {
+      // 0은 false
+      where = `WHERE id < ${lastId}`;
+    }
+    const query = `SELECT * FROM ${TABLE.TODO} ${where} order by id desc limit 0, ${len}`;
+    const [rows] = await db.execute(query);
+    return rows;
+  } catch (e) {
+    console.log(e.message);
+    return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
   }
 };
 
 const todoController = {
-
   // create
   create: async (req) => {
-    const { title } = req.body;
-    //body check
-    if (isEmpty(title)) {
-      return resData(
-        STATUS.E100.result,
-        STATUS.E100.resultDesc,
-        moment().format("LT")
-      );
+    const { title, done } = req.body;
+    if (isEmpty(title) || isEmpty(done)) {
+      return resData(STATUS.E100.result, STATUS.E100.resultDesc, moment().format('LT'));
     }
 
     try {
-      //insert
-      const query = `INSERT INTO ${TABLE.TODO} (title) VALUES (?)`;
-      const values = [title];
+      const query = `INSERT INTO todo (title, done) VALUES (?,?)`;
+      const values = [title, done];
       const [rows] = await db.execute(query, values);
       if (rows.affectedRows == 1) {
         return resData(
           STATUS.S200.result,
           STATUS.S200.resultDesc,
-          moment().format("LT"),
-          { id: rows.insertId }
+          moment().format('LT'),
         );
       }
     } catch (e) {
       console.log(e.message);
-      return resData(
-        STATUS.E300.result,
-        STATUS.E300.resultDesc,
-        moment().format("LT")
-      );
+      return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
     }
   },
 
   // list
   list: async (req) => {
+    // 화살표함수는 es6문법 this접근안됨
     const totalCount = await getTotal();
     const list = await getList(req);
     if (totalCount > 0 && list.length) {
       return resData(
         STATUS.S200.result,
         STATUS.S200.resultDesc,
-        moment().format("LT"),
+        moment().format('LT'),
         { totalCount, list }
       );
     } else {
-      return resData(
-        STATUS.S201.result,
-        STATUS.S201.resultDesc,
-        moment().format("LT")
-      );
+      return resData(STATUS.S201.result, STATUS.S201.resultDesc, moment().format('LT'));
     }
   },
 
@@ -125,11 +99,7 @@ const todoController = {
     const { id } = req.params; // url /로 들어오는것
     const { title, done } = req.body;
     if (isEmpty(id) || isEmpty(title) || isEmpty(done)) {
-      return resData(
-        STATUS.E100.result,
-        STATUS.E100.resultDesc,
-        moment().format("LT")
-      );
+      return resData(STATUS.E100.result, STATUS.E100.resultDesc, moment().format('LT'));
     }
 
     try {
@@ -140,16 +110,12 @@ const todoController = {
         return resData(
           STATUS.S200.result,
           STATUS.S200.resultDesc,
-          moment().format("LT")
+          moment().format('LT')
         );
       }
     } catch (e) {
       console.log(e.message);
-      return resData(
-        STATUS.E300.result,
-        STATUS.E300.resultDesc,
-        moment().format("LT")
-      );
+      return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
     }
   },
 
@@ -157,11 +123,7 @@ const todoController = {
   delete: async (req) => {
     const { id } = req.params; // url /로 들어오는것
     if (isEmpty(id)) {
-      return resData(
-        STATUS.E100.result,
-        STATUS.E100.resultDesc,
-        moment().format("LT")
-      );
+      return resData(STATUS.E100.result, STATUS.E100.resultDesc, moment().format('LT'));
     }
     const cnt = await getSelectOne(id);
     try {
@@ -169,7 +131,7 @@ const todoController = {
         return resData(
           STATUS.E100.result,
           STATUS.E100.resultDesc,
-          moment().format("LT")
+          moment().format('LT')
         );
       }
       const query = `DELETE FROM ${TABLE.TODO} WHERE id = ?;`;
@@ -179,53 +141,56 @@ const todoController = {
         return resData(
           STATUS.S200.result,
           STATUS.S200.resultDesc,
-          moment().format("LT")
+          moment().format('LT')
         );
       }
     } catch (e) {
       console.log(e.message);
-      return resData(
-        STATUS.E300.result,
-        STATUS.E300.resultDesc,
-        moment().format("LT")
-      );
+      return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
     }
     return rows;
   },
 
+  //retet
   reset: async (req) => {
+    // truncate
     try {
-      const title = req.body.title;
-      const len = req.body.len || 100;
-      const done = req.body.done || "N";
-      const truncateSql = `TRUNCATE TABLE ${TABLE.TODO}`;
-      await db.execute(truncateSql);
+      const query = `TRUNCATE TABLE ${TABLE.TODO};`;
+      await db.execute(query);
+    } catch (error) {
+      console.log(e.message);
+      return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
+    }
 
-      // INSERT INTO vue.todo (mb_id,title,done) VALUES (0, 'title test','Y');
-      let query = `INSERT INTO todo (title, done) values `;
-      const arr = [];
-
-      for (let i = 1; i <= len; i++) {
-        arr.push(`('${title}_${i}','${done}')`);
+    // insert
+    const { title } = req.body;
+    const done = req.body.done || "N";
+    const len = req.body.len || 100;
+    if (isEmpty(title)) {
+      return resData(STATUS.E100.result, STATUS.E100.resultDesc, moment().format('LT'));
+    }
+    try {
+      //더미쌓기 타이틀에 1씩추가하면서 인서트하기
+      let query = `INSERT INTO todo (title, done) VALUES `; //.values( data1 ), (data2),,,
+      let arr = [];
+      for (let i = 0; i < len; i++) {
+        arr.push(`('${title}_${i}', '${done}')`);
       }
-
       query = query + arr.join(",");
       const [rows] = await db.execute(query);
-      if (rows.affectedRows == len) {
+
+      if (rows.affectedRows != 0) {
         return resData(
           STATUS.S200.result,
           STATUS.S200.resultDesc,
-          moment().format("LT")
+          moment().format('LT')
         );
       }
     } catch (e) {
       console.log(e.message);
-      return resData(
-        STATUS.E300.result,
-        STATUS.E300.resultDesc,
-        moment().format("LT")
-      );
+      return resData(STATUS.E300.result, STATUS.E300.resultDesc, moment().format('LT'));
     }
   },
 };
+
 module.exports = todoController;
